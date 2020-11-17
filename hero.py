95f76ad47a1c8106de925807475e9c2fb4459319
Ali Shah <s.ashah@outlook.com>
2020-11-17 17:05:25 -0500

    [Update] Finished tutorial page 2

--- hero.py
+++ hero.py
@@ -2,3 +2,23 @@
 # Ali Shah | Nov. 17, 2020
 # CS1.1 Assignment 2: Superhero Team Dueler
 
+from random import choice
+
+class Hero:
+    """Class for hero objects."""
+
+    def __init__(self, name, starting_health=100):
+        """Constructor for hero instances. Starting health = 100."""
+        self.name = name
+        self.starting_health = starting_health
+        self.current_health = starting_health
+
+    def fight(self, opponent):
+        """Duels heroes (self and opponent)."""
+        winner = choice([self.name, opponent.name])
+        print(f"{winner} won!")
+
+if __name__ == "__main__":
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+    hero1.fight(hero2)
